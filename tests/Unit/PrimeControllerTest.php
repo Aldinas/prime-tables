bd82ec0021c2806e6c642544af519a9ccfa463ce
Jason Newport <jason@closedonsundays.co.uk>
2018-02-03 14:36:54 +0000

    Further tweaks and assertions added to tests to make them a bit more comprehensive, as well as remove a false true assertion.

--- tests/Unit/PrimeControllerTest.php
+++ tests/Unit/PrimeControllerTest.php
@@ -20,7 +20,8 @@ class PrimeControllerTest extends TestCase
       $number = 5;
       $primeCon = new PrimeController;
       $result = $primeCon->IsPrime($number);
-      $this->assertTrue(true, $number . " was not identified as a Prime number");
+      // We should get a true back, else we have gone very very wrong.
+      $this->assertTrue($result, $number . " was not identified as a Prime number");
     }
 
     /**
@@ -33,6 +34,8 @@ class PrimeControllerTest extends TestCase
       $assertion = 7;
       $primeCon = new PrimeController;
       $result = $primeCon->GetPrimeNumbers($numOfPrimes);
+      // We want to check for both our assertion number (which we have specified will be 7, and should be present) as well as ensuring
+      // that the array is the correct length (5).
       $this->assertContains($assertion,$result,$assertion . " not found in list of first " . $numOfPrimes . " prime numbers.");
       $this->assertCount($numOfPrimes, $result, "Returned list does not contain the expected " . $numOfPrimes . " elements.");
     }
@@ -42,10 +45,14 @@ class PrimeControllerTest extends TestCase
      */
     public function testLargePrimeNumbers()
     {
-      // We want an array of the first 5 primes.
+      // We want an array of the first 1000 primes.
       $numOfPrimes = 1000;
+      $assertion = 7919;
       $primeCon = new PrimeController;
       $result = $primeCon->GetPrimeNumbers($numOfPrimes);
+      // Simple test, we want 1000 items, did we get all of them?
       $this->assertCount($numOfPrimes, $result, "Returned list does not contain the expected " . $numOfPrimes . " elements.");
+      // As a safety check, lets make sure that the array also contains the 1000th prime number (7919)
+      $this->assertContains($assertion,$result,$assertion . " not found in list of first " . $numOfPrimes . " prime numbers.");
     }
 }
